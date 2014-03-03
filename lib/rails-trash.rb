baaf2621fd1b58fd45838e37424079b4d7ce0b72
Jarred <jtrost@gmail.com>
2014-03-03 13:07:54 -0600

    Refactored module

--- lib/rails-trash.rb
+++ lib/rails-trash.rb
@@ -1,61 +1,73 @@
-require 'active_support/concern'
-
 module Rails
   module Trash
 
-    extend ActiveSupport::Concern
-
-    included do
-      alias_method_chain :destroy, :trash
+    def self.included(base)
+      base.extend ClassMethods
     end
 
     module ClassMethods
-
-      def deleted(field = nil, value = nil)
-        deleted_at = Arel::Table.new(self.table_name)[:deleted_at]
-        data = unscoped
-        data = data.where(field => value) if field && value
-        data.where(deleted_at.not_eq(nil))
+      
+      def has_trash
+        extend ClassMethodsMixin
+        include InstanceMethods
+        alias_method_chain :destroy, :trash
       end
 
-      def find_in_trash(id)
-        deleted.find(id)
-      end
+      module ClassMethodsMixin
 
-      def restore(id)
-        find_in_trash(id).restore
-      end
+        def active
+          where(:deleted_at => nil)
+        end
 
+        def deleted
+          where("`deleted_at` IS NOT NULL")
+        end
+
+        def find_in_trash(id)
+          deleted.find(id)
+        end
+
+        def restore(id)
+          deleted.find(id).restore
+        end
+
+      end
+    
     end
 
-    def destroy_with_trash
-      return destroy_without_trash if @trash_is_disabled
-      deleted_at = Time.now.utc
-      self.update_attribute(:deleted_at, deleted_at)
-      self.class.reflect_on_all_associations(:has_many).each do |reflection|
-        if reflection.options[:dependent].eql?(:destroy)
-          self.send(reflection.name).update_all(:deleted_at => deleted_at)
+    module InstanceMethods
+
+      def destroy_with_trash
+        return destroy_without_trash if @trash_is_disabled
+        deleted_at = Time.now.utc
+        self.update_attribute(:deleted_at, deleted_at)
+        self.class.reflect_on_all_associations(:has_many).each do |reflection|
+          if reflection.options[:dependent].eql?(:destroy)
+            self.send(reflection.name).update_all(:deleted_at => deleted_at)
+          end
         end
       end
-    end
 
-    def restore
-      self.update_attribute(:deleted_at, nil)
-    end
-
-    def disable_trash
-      save_val = @trash_is_disabled
-      begin
-        @trash_is_disabled = true
-        yield if block_given?
-      ensure
-        @trash_is_disabled = save_val
+      def restore
+        self.update_attribute(:deleted_at, nil)
       end
-    end
 
-    def trashed?
-      deleted_at.present?
-    end
+      def disable_trash
+        save_val = @trash_is_disabled
+        begin
+          @trash_is_disabled = true
+          yield if block_given?
+        ensure
+          @trash_is_disabled = save_val
+        end
+      end
 
+      def trashed?
+        deleted_at.present?
+      end
+
+    end
   end
 end
+
+ActiveRecord::Base.send :include, Rails::Trash
