514d9073cce254ab615563927e48edfbedece2b8
Jarred <jtrost@gmail.com>
2014-03-04 10:23:01 -0600

    Bug fix

--- lib/rails-trash.rb
+++ lib/rails-trash.rb
@@ -63,8 +63,8 @@ module Rails
         def restore_associations
           self.class.reflect_on_all_associations(:has_many).each do |reflection|
             if reflection.options[:dependent] == :destroy
-              associations = self.send(reflection.name).deleted
-              associations.update_all(:deleted_at => nil) if associations.count > 0
+              associations = self.send(reflection.name)
+              associations.deleted.update_all(:deleted_at => nil) if associations.deleted.count > 0
             end
           end
         end
